d9806ca64965b7171edc7c277e76c6d2bbcb27dc
Carter Wright <wrig0476@algonquinlive.com>
2023-08-04 16:37:40 -0400

    updated currency converter but cant get api response to print to EditText

--- app/src/main/java/algonquin/cst2335/mobilefinalproject/CurrencyConverter.java
+++ app/src/main/java/algonquin/cst2335/mobilefinalproject/CurrencyConverter.java
@@ -1,15 +1,31 @@
 package algonquin.cst2335.mobilefinalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
 
 public class CurrencyConverter extends AppCompatActivity {
 
     private ImageButton imageButton;
+    private Spinner fromSpinner;
+    private Spinner toSpinner;
+    private EditText amountEditText;
+    private EditText convertAmount;
+    private Button convertButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +33,81 @@ public class CurrencyConverter extends AppCompatActivity {
         setContentView(R.layout.activity_currency_converter);
 
         imageButton = findViewById(R.id.imgButton);
+        fromSpinner = findViewById(R.id.fromSpinner);
+        toSpinner = findViewById(R.id.toSpinner);
+        amountEditText = findViewById(R.id.amountEditText);
+        convertAmount = findViewById(R.id.convertAmount);
+        convertButton = findViewById(R.id.convertButton);
 
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CurrencyConverter.this, MainActivity.class);
-                startActivity(intent);
-            }
+        imageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(CurrencyConverter.this, MainActivity.class);
+            startActivity(intent);
         });
+
+        // Define the currency options
+        String[] currencyOptions = {"AUD", "CAD", "USD"};
+
+        // Create ArrayAdapter instances with the currency options
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, currencyOptions);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter instances to the Spinners
+        fromSpinner.setAdapter(adapter);
+        toSpinner.setAdapter(adapter);
+
+
+        convertButton.setOnClickListener(v -> {
+
+
+            // Perform currency conversion here
+            performCurrencyConversion();
+
+            //double editText1 = Double.parseDouble(String.valueOf(amountEditText.getText())) * 10;
+            //amount.setText(String.valueOf(editText1));
+        });
+
     }
+    private void performCurrencyConversion () {
+        String fromCurrency = fromSpinner.getSelectedItem().toString();
+        String toCurrency = toSpinner.getSelectedItem().toString();
+        double currencyAmount; // Initialize amount to a default value
+
+        currencyAmount = Double.parseDouble(amountEditText.getText().toString());
+
+        // Construct the API URL with the selected currencies and amount
+        String apiUrl = "https://api.getgeoapi.com/v2/currency/convert?format=json"
+                + "&from=" + fromCurrency
+                + "&to=" + toCurrency
+                + "&amount=" + currencyAmount
+                + "&api_key=2011c0442fd66c69fb987e9e3a20f85e05d0a37a"
+                + "&format=json";
+
+
+        // Make the API request using Volley
+        // Handle API request errors here
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, apiUrl, null,
+                response -> {
+                    // Handle the API response here
+                    try {
+                        // Parse the JSON response and extract the converted amount
+                        String rateForAmountString = response.getString("rate_for_amount");
+
+                        Toast.makeText(CurrencyConverter.this, "converted amount: " + rateForAmountString, Toast.LENGTH_LONG).show();
+
+                        // Update the "convertAmount" EditText with the converted value
+                        //EditText convertAmountEditText = findViewById(R.id.convertAmount);
+                        double convertedAmount = Double.parseDouble(rateForAmountString);
+
+                        convertAmount.setText(String.valueOf(convertedAmount));
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                Throwable::printStackTrace);
+
+        // Add the request to the RequestQueue (Volley handles it automatically)
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
+    }
+
 }
